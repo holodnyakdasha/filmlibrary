518cf14e05ca6f6128c75bf7d2f6ad0bfb3d11de
holodnyakdasha <holodnyakdasha@gmail.com>
2019-04-26 07:32:52 +0300

    added 404 error component

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import Router from 'vue-router'
 import Home from '../components/Home/Home.vue'
 import Login from '../components/Login/Login.vue'
 import Registration from '../components/Registration/Registration.vue'
+import Error404 from '../components/Errors/Error404.vue'
 
 Vue.use(Router)
 
@@ -23,6 +24,11 @@ export default new Router({
       path: '/registration',
       name: 'Registration',
       component: Registration
+    },
+    {
+      path:'/404',
+      name:'Error404',
+      component: Error404
     }
   ]
 })
